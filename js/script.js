0963533dee25c246ba515a77e7c4871cb762a4ab
Shubhangi <sbisaria@tavisca.com>
2017-09-02 11:37:18 +0530

    add fully responsive bootstrap Expedia site

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 $( function() {
-    $( "#datepicker" ).datepicker({    	
-    	changeMonth: true,
-		changeYear: true,
-		minDate: '0m+1d',
-		showAnim: "fadeIn",
-	            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    $( "#datepicker" ).datepicker({     
+      changeMonth: true,
+    changeYear: true,
+    minDate: '0m+1d',
+    showAnim: "fadeIn",
+              dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     }
     );
   } );
 $( function() {
     $( "#tabs" ).tabs();
   } );
-// $(document).ready(function(){
-//     $("#btn-flight").click(function(){
-//         $("flight").hide();
-//     });
-//     $("#btn-hotel").click(function(){
-//         $("hotel").show();
-//     });
-// });
